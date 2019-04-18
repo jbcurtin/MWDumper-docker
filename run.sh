e75beb5769932b2d56d25ffde1db3832a249a7b2
Joseph Curtin <jbcurtincode@gmail.com>
2019-04-19 05:56:52 +0900

    mysql 1.5

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # docker run -v $PWD/enwiki-sql:/sql -v $PWD/enwiki-dump:/dumps
 #   mwdumper /app/run.sh enwiki-20190320-pages-meta-current27.xml-p53163462p54663462.bz2
 
-java -jar /app/mwdumper.jar --format=pgsql:1.5 /dumps/$1 > /sql/$1.sql
+java -jar /app/mwdumper.jar --format=mysql:1.5 /dumps/$1 > /sql/$1.sql
